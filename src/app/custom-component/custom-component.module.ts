56870c5520e8844600961a7cba1803c09bc6d709
LaukikOnWinLab <22937426+LRagji@users.noreply.github.com>
2019-11-01 21:18:36 +0530

    Added Side nav

--- src/app/custom-component/custom-component.module.ts
+++ src/app/custom-component/custom-component.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { CustomComponentRoutingModule } from './custom-component-routing.module';
 import { CustomComponentComponent } from './custom-component.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 @NgModule({
   declarations: [CustomComponentComponent],
   imports: [
     CommonModule,
-    CustomComponentRoutingModule
+    CustomComponentRoutingModule,
+    MatSidenavModule
   ]
 })
 export class CustomComponentModule { }
