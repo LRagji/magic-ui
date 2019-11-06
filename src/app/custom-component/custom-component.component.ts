332cb8a1c4433a17e1eb15ca3abdf8f186c54509
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2019-11-06 13:36:18 +0530

    Temmp Code 1

--- src/app/custom-component/custom-component.component.ts
+++ src/app/custom-component/custom-component.component.ts
@@ -23,13 +23,32 @@ export class CustomComponentComponent implements OnInit {
 
   @ViewChild('sidenav', { static: true }) sidenav;
   private _selectedTile: Tile;
-  basicElements: Array<magicElement> = [{name:"button",markup:'<button mat-raised-button color="primary">Basic</button>'},{name:"textbox",markup:""}]
+  basicElements: Array<magicElement> = [{ name: "button", markup: '<button mat-raised-button color="primary">Basic</button>' }, { name: "textbox", markup: "" }]
   tiles: Array<Tile> = [{ index: 0, text: 'One', cols: 6, rows: 3, color: 'lightblue', content: '' },
   { index: 1, text: 'Two', cols: 6, rows: 3, color: 'lightblue', content: '' }];
 
   constructor() { }
 
   ngOnInit() {
+    const someCode = `class mat_button {
+      constructor() {
+          this.package = {
+              execute: ['npx ng add @angular/material --interactive=false --theme=custom --gestures=true --animations=true'],
+              moduleImports: [{ moduleName: 'MatButtonModule', link: '@angular/material/button' }]
+          };
+          this.defaultProperties = {
+              text: "Hello World"
+          };
+          this.template = this.template.bind(this);
+      }
+
+      async template(props, layoutBuilder) {
+          return \`< button mat-raised - button color = "primary" > \${ props.text } </button>\`;
+  }
+};
+return new mat_button();`;
+    const mat_button = new Function(someCode)();
+    console.log(mat_button.template(mat_button.defaultProperties, () => ''));
   }
 
   addElement(elementMarkup: string) {
